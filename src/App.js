c6d0802c6ae0e6e3d9184a1bd2a4331f246a275d
Okibolat <okibolat@MacBook-Pro-Shamil.local>
2022-07-13 13:55:32 +0300

    asdasd

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import React, { useEffect } from 'react';
-import { Widget, addResponseMessage } from 'react-chat-widget';
+// import { Widget, addResponseMessage } from 'react-chat-widget';
 
 import 'react-chat-widget/lib/styles.css';
 import './app.css';
 
 function App({ domElement }) {
-  useEffect(() => {
-    addResponseMessage('Donate to the cause!');
-  }, []);
+  // useEffect(() => {
+  //   addResponseMessage('Donate to the cause!');
+  // }, []);
 
-  
-
-    return (
-      <div className="App">
-        <Widget
-          title="Donations Widget"
-          subtitle="Welcome"
-        />
-      </div>
-    );
+  return (
+    <div className="App">
+      nflnsnm lorem afalsflkanfksnkjnfkjdsn
+      {/* <Widget
+        title="Donations Widget"
+        subtitle="Welcome"
+      /> */}
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('widjet'));
+const root = ReactDOM.createRoot(document.getElementById('widget-container'));
 root.render(
     <App/>
 );
